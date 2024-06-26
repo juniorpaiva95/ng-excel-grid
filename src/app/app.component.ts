57c95ce58bbdb10a31f8811b500a47154d95aa46
Junior Paiva <juniorpaiva95@gmail.com>
2024-06-25 23:01:47 -0300

    chore: remove excel-table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ExcelGridComponent } from './excel-grid/excel-grid.component';
-import { ExcelTableComponent } from './excel-table/excel-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ExcelTableComponent, ExcelGridComponent],
+  imports: [RouterOutlet, ExcelGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
